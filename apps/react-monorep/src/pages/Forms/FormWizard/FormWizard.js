cc37a853411fd554923254a04977150f43cc4851
basit <cool.saa733@gmail.com>
2024-10-30 14:56:12 +0500

    velzon theme integrate into NX MonoRepo

---
+++ apps/react-monorep/src/pages/Forms/FormWizard/FormWizard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FormWizard = () => {
+  return (
+    <div>FormWizard</div>
+  )
+}
+
+export default FormWizard
--- apps/react-monorep/webpack.config.js
+++ apps/react-monorep/webpack.config.js
@@ -1,13 +1,13 @@
 const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
-const { NxReactWebpackPlugin } = require('@nx/react/webpack-plugin');
 const { join } = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   output: {
     path: join(__dirname, '../../dist/apps/react-monorep'),
   },
   devServer: {
-    port: 4200,
+    port: 4100,
     historyApiFallback: {
       index: '/index.html',
       disableDotRule: true,
@@ -18,7 +18,7 @@ module.exports = {
     new NxAppWebpackPlugin({
       tsConfig: './tsconfig.app.json',
       compiler: 'babel',
-      main: './src/main.tsx',
+      main: './src/index.js',
       index: './src/index.html',
       baseHref: '/',
       assets: ['./src/favicon.ico', './src/assets'],
@@ -26,10 +26,12 @@ module.exports = {
       outputHashing: process.env['NODE_ENV'] === 'production' ? 'all' : 'none',
       optimization: process.env['NODE_ENV'] === 'production',
     }),
-    new NxReactWebpackPlugin({
-      // Uncomment this line if you don't want to use SVGR
-      // See: https://react-svgr.com/
-      // svgr: false
-    }),
+    new webpack.DefinePlugin({
+      global: 'window'
+    })
+    
   ],
+  stats: {
+    warnings: false, 
+  },
 };
